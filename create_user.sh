ff984f92f81c143074ac67db19c9c17df61866bd
Fruela Pérez <fruela.perez@protonmail.com>
2020-02-12 02:32:41 +0000

    Versión más currada del script de creación de usuarios

--- create_user.sh
+++ create_user.sh
@@ -5,11 +5,28 @@ if [ $# -lt 2 ] ;then
 	exit 1
 fi
 
-sudo useradd -m $1
-echo -e "$2\n$2" | sudo passwd -q 2>&1 $NEW_USER
+#grep -c '^$1:' /etc/passwd > /dev/null 2&>1
+#getent passwd $1 > /dev/null 2&>1
 
-if [ "$3" = "sudoer" ] ;then
-	sudo usermod -a -G sudo $1
+if id "$1" >/dev/null 2>&1; then
+
+#if [ $? -eq 1 ]; then
+	echo "El usuario $1 ya existe."
+	echo
+	exit 2
+else
+	sudo useradd -m $1
+	echo -e "$2\n$2" | sudo passwd -q 2>&1 $NEW_USER 2>/dev/null
+
+	if [ "$3" = "sudoer" ] ;then
+		sudo usermod -a -G sudo $1
+	elif [ -n "$3" ]; then
+		echo "El tercer parámetro no es válido, sólo se admite la cadena 'sudoer'."
+		echo "El usuario $1 no se añadirá a los sudoers"
+		echo
+	else
+		echo
+	fi
 fi
 
 echo
