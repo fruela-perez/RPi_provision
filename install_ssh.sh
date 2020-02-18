7f9bdcef7ac7b157f28df28147f4cebcc1f43245
Fruela Pérez <fruela.perez@protonmail.com>
2020-02-18 16:02:58 +0100

    sudo touch /etc/skel/.hushlogin

--- install_ssh.sh
+++ install_ssh.sh
@@ -12,7 +12,7 @@ if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
 	sudo systemctl start ssh
 	# Remove GNU licence and 'no warranty' when logging into ssh
 	touch ~/.hushlogin
-	touch /etc/skel/.hushlogin
+	sudo touch /etc/skel/.hushlogin
 else
     echo Omitiendo la instalación del servidor SSH
 fi
