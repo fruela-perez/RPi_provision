39e258f5d4aa1cf7520c4931892d286dcc5b4e42
Fruela Pérez <fruela.perez@protonmail.com>
2020-03-08 13:27:57 +0100

    Instalación de Textpattern

--- install_textpattern.sh
+++ install_textpattern.sh
@@ -4,21 +4,37 @@ echo "+-------------------------+"
 echo "|  Instalar  Textpattern  |"
 echo "+-------------------------+"
 echo
-cd
 
-sudo apt-get install -y gtkhash
+echo -n "¿Instalar Textpattern? [s/N] "
+read respuesta
 
-wget https://textpattern.com/File+download/91/textpattern-$TEXTPATTERNS_VERSION.zip
+if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
 
-sha256file=$(sha256sum textpattern-$VERSION.zip | cut -b 1-65) 
+	if [ -z $TEXTPATTERN_VERSION ] ;then
+		source $(dirname $0)/settings.sh
+	fi
+	
+	cd
 
-if [ $sha256file = $TEXTPATTERNS_CHECKSUM ] ;then
-	unzip textpattern-$TEXTPATTERNS_VERSION.zip
-	rm textpattern-$TEXTPATTERNS_VERSION.zip
+	sudo apt-get install -y gtkhash
 
-	mysql -u root -p$MYSQL_ROOT_PWD < create_textpattern_database.sql	
+	wget $TEXTPATTERN_BASE_URL/textpattern-$TEXTPATTERN_VERSION.zip
+
+	sha256file=$(sha256sum textpattern-$TEXTPATTERN_VERSION.zip | cut -b 1-65) 
+
+	if [ $sha256file = $TEXTPATTERN_CHECKSUM ] ;then
+		unzip textpattern-$TEXTPATTERN_VERSION.zip
+		rm textpattern-$TEXTPATTERN_VERSION.zip
+
+		mysql -u root -p$MYSQL_ROOT_PWD < create_textpattern_database.sql	
+	else
+		echo "Ha fallado la comprobación de la integridad de textpattern-$TEXTPATTERN_VERSION.zip :("
+		echo "Abortando."
+	fi
+
 else
-	echo "Ha fallado la comprobación de la integridad de textpattern-$TEXTPATTERNS_VERSION.zip :("
-	echo "Abortando."
+    echo Omitiendo la instalación de Textpattern
 fi
+
+
 
