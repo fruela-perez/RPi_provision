d6e479668fc3ede90164f9e61dc0e46a1d658dd2
Fruela Pérez <fruela.perez@protonmail.com>
2020-03-08 23:29:52 +0100

    Update install_GoAccess.sh

--- install_GoAccess.sh
+++ install_GoAccess.sh
@@ -4,6 +4,9 @@ echo "+-------------------------+"
 echo "| GoAccess - Apache Stats |"
 echo "+-------------------------+"
 echo
+
+SETTINGS=$(readlink -f $(dirname $0))/settings.sh
+
 echo -n "¿Instalar GoAccess? [s/N] "
 read respuesta
 
@@ -11,6 +14,10 @@ if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
     echo Instalando GoAccess...
     echo
     sudo apt-get update && sudo apt-get install -y libgeoip-dev libncursesw5-dev
+
+	if [ -z $GOACCESS_VERSION ] ;then
+		source $SETTINGS
+	fi
 
     cd
     wget https://tar.goaccess.io/goaccess-$GOACCESS_VERSION.tar.gz
@@ -21,6 +28,16 @@ if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
     ./configure --enable-utf8 --enable-geoip=legacy
     make
     sudo make install
+
+	echo -n "¿Instalar scripts para generar informes y crear cron jobs? [s/N] "
+	read respuesta
+
+	if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
+	    echo ToDo.
+	else
+	    echo Omitiendo el proceso.
+	fi
+
 else
     echo Omitiendo la instalación.
 fi
