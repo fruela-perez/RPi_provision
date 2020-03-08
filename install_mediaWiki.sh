869fb679ead57fe146ec26137eb123f887cea3f7
Fruela Pérez <fruela.perez@protonmail.com>
2020-03-08 20:35:26 +0100

    Instalación independiente de MediaWiki

--- install_mediaWiki.sh
+++ install_mediaWiki.sh
@@ -8,9 +8,14 @@ echo -n "Instalar MediaWiki $MEDIAWIKI_VERSION? [s/N] "
 read respuesta
 
 if [ "$respuesta" != "${respuesta#[Ss]}" ] ;then
+
+	if [ -z $MEDIAWIKI_VERSION ] ;then
+		source $(dirname $0)/settings.sh
+	fi
+
 	cd
 	wget https://releases.wikimedia.org/mediawiki/1.34/mediawiki-$MEDIAWIKI_VERSION.tar.gz
-	tar -xvf mediawiki-$VERSION.tar.gz
+	tar -xvf mediawiki-$MEDIAWIKI_VERSION.tar.gz
 	rm mediawiki-$MEDIAWIKI_VERSION.tar.gz*
 	mv mediawiki-$MEDIAWIKI_VERSION $MEDIAWIKI_DESTINO 
 else
